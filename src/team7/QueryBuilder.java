13c19f338e8c33dffb4de503f150ce81f0f9770f
malikvivek <vm.malikvivek@gmail.com>
2017-02-19 16:23:29 -0500

    Phase 2 Deliverable, Interfaces

---
+++ src/team7/QueryBuilder.java
@@ -0,0 +1,23 @@
+package team7;
+
+import java.util.Map;
+
+public interface QueryBuilder {
+
+	/*
+	 * createQuery method receives a key-value pair for search parameters
+	 * Returns a string query
+	 */
+	public String createQuerry(Map<String, String> searchParam);
+	
+	/*
+	 * validate the query and look for evil inputs for enhanced security
+	 */
+	
+	public boolean validateQuery(String searchQuery);
+	
+	/*
+	 * send the query to Database
+	 */
+	public void sendQuery();
+}
