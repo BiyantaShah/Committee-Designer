e1ecd10b160be385ba6f2ca2ce43d089f6503eb2
sravani <beeram.s@husky.neu.edu>
2017-03-29 06:09:27 -0400

    modified parse test cases

--- src/test/java/com/team7/ImplementParseDatabaseTest.java
+++ src/test/java/com/team7/ImplementParseDatabaseTest.java
@@ -1,6 +1,8 @@
 package com.team7;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.SQLException;
 
 import javax.xml.bind.JAXBException;
@@ -11,25 +13,29 @@ import junit.framework.TestCase;
 public class ImplementParseDatabaseTest extends TestCase{
 
 	@Test
-	public void testParseSuccess() throws JAXBException, SQLException {
+	public void testParseSuccess() throws JAXBException, SQLException, IOException {
 		
 		// to test the successful parsing of XML file
-		File file = new File("input/positiveTest.xml");
-		ImplementParseDatabase parse = new ImplementParseDatabase();
+	    File file = new File("test.xml");
+	    FileWriter writer = new FileWriter(file);
+	    writer.write("<?xml version='1.0' encoding='ISO-8859-1'?><dblp><garbage><test>This is a test</test></garbage></dblp>"); 
+		writer.close();
+	    ImplementParseDatabase parse = new ImplementParseDatabase();
 		String result = parse.parseXml(file);
 		assertEquals("success", result);
-
 	}
 	
 	@Test
-	public void testParseFailure() throws JAXBException, SQLException {
+	public void testParseFailure() throws JAXBException, SQLException, IOException {
 		
 		// to test the failure of parsing of XML file
-		File file = new File("input/negativeTest.xml");
+	    File file = new File("test.xml");
+	    FileWriter writer = new FileWriter(file);
+	    writer.write("<?xml version='1.0' encoding='ISO-8859-1'?><dblp><garbage><test>'Negative'</test></garbage></dblp>"); 
+		writer.close();
 		ImplementParseDatabase parse = new ImplementParseDatabase();
 		String result = parse.parseXml(file);
 		assertEquals("failure", result);
 		
 	}
-
 }
