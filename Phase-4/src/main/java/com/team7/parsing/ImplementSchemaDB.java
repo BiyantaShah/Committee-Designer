365ea6483eb32b0adab5591e10a91ed001eeeded
sravani <beeram.s@husky.neu.edu>
2017-04-17 01:00:37 -0400

    modified username and password column's in User to not null

--- Phase-4/src/main/java/com/team7/parsing/ImplementSchemaDB.java
+++ Phase-4/src/main/java/com/team7/parsing/ImplementSchemaDB.java
@@ -76,8 +76,8 @@ public class ImplementSchemaDB implements SchemaDB {
 			//creating user table
 			sql = "CREATE TABLE IF NOT EXISTS User " +
 					"(id       INTEGER      AUTO_INCREMENT NOT NULL, " +
-					" username VARCHAR(255) UNIQUE, " + 
-					" password VARCHAR(255), " + 
+					" username VARCHAR(255) UNIQUE NOT NULL, " + 
+					" password VARCHAR(255) NOT NULL, " + 
 					" role     VARCHAR(255), " + 
 					" confName VARCHAR(255),"+
 					" PRIMARY  KEY(id))"; 
